8e79fd7e3a526142613b6a564175586214bfd0e7
Kythadrin <vadimsiric@gmail.com>
2023-01-18 21:15:44 +0200

    IOmplemented JWT token on client side

--- src/Listeners/AuthenticationSuccessListener.php
+++ src/Listeners/AuthenticationSuccessListener.php
@@ -1,1 +1,44 @@
 <?php
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use Exception;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationSuccessResponse;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class AuthenticationSuccessListener
+{
+    private string $jwtTokenTTL;
+
+    private bool $cookieSecure = false;
+
+    public function __construct($ttl)
+    {
+        $this->jwtTokenTTL = $ttl;
+    }
+
+    /**
+     * @param AuthenticationSuccessEvent $event
+     * @return JWTAuthenticationSuccessResponse
+     * @throws Exception
+     */
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): JWTAuthenticationSuccessResponse
+    {
+        /** @var JWTAuthenticationSuccessResponse $response */
+        $response = $event->getResponse();
+        $data = $event->getData();
+        $tokenJWT = $data['token'];
+        unset($data['token']);
+        unset($data['refresh_token']);
+        $event->setData($data);
+
+        $response->headers->setCookie(new Cookie('BEARER', $tokenJWT, (
+        new \DateTime())
+            ->add(new \DateInterval('PT' . $this->jwtTokenTTL . 'S')), '/', null, $this->cookieSecure)
+        );
+
+        return $response;
+    }
+}
